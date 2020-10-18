2751f91d9b1b46ab2c9253eae20ebc2cd1784101
Danil Demintsev <demintsievd@yandex.ru>
2020-10-18 17:23:15 +0300

    Add form with text and img inputs

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import config
-
 import flask
 
 app = flask.Flask(__name__)
 
+
 @app.route('/')
-def main_page():
-    return 'Hello, world!'
+def index():
+    return flask.render_template('index.html')
+
+
+@app.route('/ok_page')
+def ok_page():
+    return "OK"
+
+
+@app.route('/get_data', methods=['POST'])
+def get_data():
+    my_dict = flask.request.form
+    print(my_dict)
+    return flask.render_template('get_data.html', data=my_dict)
 
 if (__name__ == '__main__'):
     app.run(host=config.MY_HOST)
